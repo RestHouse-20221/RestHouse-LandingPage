bb68b917df49f2b160f51fa68f0bf0dd37044f7d
Mathyu Cardozo <68485205+xMathyu@users.noreply.github.com>
2022-06-02 10:52:45 -0500

    Fix the Js file

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,3 @@
-const { objectToString } = require("@vue/shared")
-
 /*=============== SHOW MENU ===============*/
 const showMenu = (toggleId, navId) =>{
     const toggle = document.getElementById(toggleId),
